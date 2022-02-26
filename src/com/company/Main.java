ce3de9d64b4f0774863008b2d9b5c895039662b8
Notnik_kg <dinararahatbekkyzy@gmaiil.com>
2022-02-26 16:46:42 +0600

    hw20

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+   public class Main {
+
+    public static void main(String[] args)  {
+//        Try with resources-те колдоно ала турган Car деген класс тузунуз
+//        close() деген метод "Машина жабылып жатат" деп консольго чыгаруусу керек.
+//        try unchecked exceptionдарды кармап аларды игнор кылыш керек.
+//                main методдо try блоктун ичинде drive() деген методду чакырыныз, ал консольго "Машина журуп жатат" деп чыгаруусу керек
+//
+//        Эскертуу: маселени чыгарууда try with resources колдонуу керек.
+
+        try(Car car = new Car()) {
+            car.drive();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
